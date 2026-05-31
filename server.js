5b3047c4580e939e980418e78d5936c799add507
JOLISHA ANGELIN P <jolishaangelin@gmail.com>
2026-05-31 10:53:12 +0530

    Render deployment fix

--- server.js
+++ server.js
@@ -52,6 +52,8 @@ app.get("/students", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server Running");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
